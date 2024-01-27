de83a355feb52f3924aeb4a7ca80872d1d839761
“I401” <milk7456852@gmail.com>
2024-01-27 13:23:23 +0800

    Updates

--- assets/index-7436789a.js
+++
@@ -1,1 +0,0 @@
-import"./main-28d9d09d.js";const o=document.querySelector(".blueBox");gsap.to(o,{x:500,duration:3,rotation:180,backgroundColor:"red",ease:"bounce.in"});console.log(444);
---
+++ assets/index-cf3afc5c.js
@@ -0,0 +1,1 @@
+import"./main-6c9b165e.js";const o=document.querySelector(".blueBox");gsap.to(o,{x:500,duration:3,rotation:180,backgroundColor:"red",ease:"bounce.in"});console.log(444);
